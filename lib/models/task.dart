f4f641ead14b5b45e6720c19aafdaef66d473458
Sebghat Yusuf <sebghatyusuf@gmail.com>
2020-10-10 14:13:28 +0430

    fix database

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,12 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class Task {
+  int id;
   String name;
   bool completed;
   Task({@required this.name, this.completed = false});
+  Task._withId(this.id, this.name, this.completed);
 
-
-  void toggleCompleted (){
+  void toggleCompleted() {
     completed = !completed;
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['name'] = name;
+    map['completed'] = completed;
+    return map;
+  }
+
+  Task.fromMap(Map<String, dynamic> map){
+    this.id = map['id'];
+    this.name = map['name'];
+    this.completed = getBool(map);
+  }
+
+  bool getBool(map){
+    if (json.decode(map['completed']) == 1){
+      return true;
+    }
+    return false;
   }
 }
--- lib/providers/todos_model.dart
+++ lib/providers/todos_model.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:provider_test/models/task.dart';
+import 'package:provider_test/utils/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TodosModel extends ChangeNotifier {
-  final List<Task> _tasks = [];
+  DatabaseHelper databaseHelper = DatabaseHelper();
+
+  List<Task> _tasks = [];
+
+  List<Task> _getTaskListFromDb() {
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    dbFuture.then((database) {
+      Future<List<Task>> taskListFuture = databaseHelper.getTaskList();
+      taskListFuture.then((taskList) {
+        return taskList;
+      });
+    });
+  }
+
+  TodosModel() {
+   _tasks =  _getTaskListFromDb();
+  }
 
   get allTasks => _tasks;
   get completedTasks => _tasks.where((todo) => todo.completed).toList();
   get incompleteTasks => _tasks.where((todo) => !todo.completed).toList();
 
-  void addTask(Task task) {
+  void addTask(Task task) async {
+    int result = await databaseHelper.insertTask(task);
+
     _tasks.add(task);
     notifyListeners();
   }
 
-  void toggleTodo(Task task) {
+  void toggleTodo(Task task) async {
+    int result = await databaseHelper.updateTask(task);
     final index = _tasks.indexOf(task);
     _tasks[index].toggleCompleted();
     notifyListeners();
   }
 
-  void deleteTodo(Task task) {
+  void deleteTodo(Task task) async {
+    int result = await databaseHelper.deleteTask(task.id);
     _tasks.remove(task);
     notifyListeners();
   }
